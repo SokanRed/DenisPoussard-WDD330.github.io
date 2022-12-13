259f78e3ddfa0c3628e4da8ea935fb866a177334
Denis PC Poussard <denis@sokan.fr>
2022-12-13 20:35:39 +0100

    Created the Challenges page

---
+++ Challenge/CountryApp/script.js
@@ -0,0 +1,57 @@
+const countriesContainer = document.querySelector(".countries-container");
+const btnSort = document.querySelectorAll(".btnSort");
+let countriesData = [];
+let sortMethod = "maxToMin";
+
+// console.log(countriesContainer);
+// console.log(btnSort);
+
+async function fetchCoutries() {
+    await fetch('https://restcountries.com/v3.1/all')
+        .then((res) => res.json())
+        .then((data) => countriesData = data);
+    console.log(countriesData);
+    countriesDisplay();
+};
+
+function countriesDisplay() {
+    countriesContainer.innerHTML = countriesData
+        .filter((country) =>
+            country.name.common.toLowerCase().includes(inputSearch.value.toLowerCase()))
+        .sort((a, b) => {
+            if (sortMethod === "maxToMin") {
+                return b.population - a.population;
+            } else if (sortMethod === "minToMax") {
+                return a.population - b.population;
+            } else if (sortMethod = "alpha") {
+                return a.name.common.localeCompare(b.name.common);
+            }
+        })
+        .slice(0, inputRange.value)
+        .map(
+            (country) =>
+            `
+    <div class="card">
+        <img src=${country.flags.svg} alt="${country.name.common}flag">
+        <h2>${country.name.common}</h2>
+        <h4>Capital : ${country.capital}</h4>
+        <p>Population : ${country.population.toLocaleString()}</p>
+    </div>
+    `
+        )
+        .join("")
+}
+
+window.addEventListener("load", fetchCoutries);
+inputSearch.addEventListener("input", countriesDisplay);
+inputRange.addEventListener("input", () => {
+    countriesDisplay();
+    rangeValue.textContent = inputRange.value;
+});
+btnSort.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+        // console.log(e.target.id);
+        sortMethod = e.target.id;
+        countriesDisplay();
+    })
+})
